404426a0ee18d6297c8bd399f876d9270777c735
chen7gx <gxissun@gmail.com>
2021-12-25 21:02:39 +0800

    out

--- time.sh
+++ time.sh
@@ -1,10 +1,16 @@
 START_TIME=`date +%s%N`
-
+END_TIME=`date +%s%N`
 #此处添加要执行的命令
-zokrates export-verifier
-
-
-
+for ((i=1;i<21;i++))
+do 
+	START_TIME=`date +%s%N`
+	zokrates compute-witness -a $i 10
+	zokrates generate-proof
 END_TIME=`date +%s%N` 
-EXECUTING_TIME=`expr $END_TIME - $START_TIME`
-echo $EXECUTING_TIME
+EXECUTING_TIME[$i]=`expr $END_TIME - $START_TIME`
+done
+
+for ((i=1;i<21;i++))
+do
+echo ${EXECUTING_TIME[$i]}
+done
